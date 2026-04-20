38de4129275937b4a7dad73f94936466c8536d0f
PremAgentAlliance <chetlapalleprem@agentalliance.in>
2026-04-20 13:08:52 +0530

    public ip added

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ connectDB()
 
 
 
-app.listen(3000, () => {
+// app.listen(3000, () => {
+//   console.log("Server running on port 3000");
+// });
+
+
+// to run locally and physical device
+app.listen(3000, '0.0.0.0', () => {
   console.log("Server running on port 3000");
 });
