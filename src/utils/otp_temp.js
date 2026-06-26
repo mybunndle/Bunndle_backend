50e5220402f4baaa5b29e21791635165986cfd58
Chandan <cs762585@gmail.com>
2026-06-26 13:00:13 +0530

    updated otp controller

--- src/utils/otp_temp.js
+++ src/utils/otp_temp.js
@@ -1,6 +1,6 @@
 import crypto from "crypto";
 
-export const DEFAULT_OTP = "1234";
+export const DEFAULT_OTP = "123456";
 
 export const hashOtp = (otp) =>
   crypto.createHash("sha256").update(otp).digest("hex");
